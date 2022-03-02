5eee8a5bedad0a5ca30ca80dcc6fea2f7b170c25
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-02 17:02:14 +0100

    log route only draft

---
+++ routes/log.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const res = require("express/lib/response");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Log = require("../models/Log.model");
+
+module.exports = router;
