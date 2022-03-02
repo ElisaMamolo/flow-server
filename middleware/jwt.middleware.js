0f8a222fb61eb52172dfaf90e76b3ea8c97af2b0
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-02 17:01:08 +0100

    add jwt middleware

---
+++ middleware/jwt.middleware.js
@@ -0,0 +1,33 @@
+// middleware/jwt.middleware.js
+
+const jwt = require("express-jwt");
+
+// Instantiate the JWT token validation middleware
+//we created the authentication middleware and named it isAuthenticated.
+//We instantiated it using the following options:
+const isAuthenticated = jwt({
+  secret: process.env.TOKEN_SECRET,
+  algorithms: ["HS256"],
+  requestProperty: "payload",
+  getToken: getTokenFromHeaders,
+});
+
+// Function used to extracts the JWT token from the request's 'Authorization' Headers
+function getTokenFromHeaders(req) {
+  // Check if the token is available on the request Headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    // Get the encoded token string and return it
+    const token = req.headers.authorization.split(" ")[1];
+    return token;
+  }
+
+  return null;
+}
+
+// Export the middleware so that we can use it to create a protected routes
+module.exports = {
+  isAuthenticated,
+};
