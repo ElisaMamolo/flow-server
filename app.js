a004d9e2de8070c2f10479d7ce1e549b52544e84
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-03-02 17:00:40 +0100

    add routes to app.js

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ require("./config")(app);
 const allRoutes = require("./routes/index.routes");
 app.use("/api", allRoutes);
 
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
+const acquariumRouter = require("./routes/acquarium.routes");
+app.use("/acquarium", acquariumRouter);
+
+const logRouter = require("./routes/log.routes");
+app.use("/log", logRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
